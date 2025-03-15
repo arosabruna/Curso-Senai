8f98a2dd9a92e9e17428c6e060f52f68ba72dd32
Bruna Rosa <almeidarosabruna@gmail.com>
2025-03-15 17:37:54 -0300

    Delete Aula 2 Bruna directory

--- Aula 2 Bruna/calculadora.py
+++
@@ -1,6 +0,0 @@
-# Calculadora Tabuada 1 ao 10 #
-
-for y in range(1, 11):
-  for x in range(1, 11):
-    print(f"{y} x {x} = {x*y}")
-  print('')
--- Aula 2 Bruna/palavra.py
+++
@@ -1,9 +0,0 @@
-# Identificar Palavra Palíndroma #
-
-p = input("Escreva uma palavra: ").upper()
-p1 = p[::-1]
-
-if p == p1:
-  print(f"A palavra {p} é palíndroma.")
-else:
-  print(f"A palavra {p} não é palíndroma.")
